d9d11707f3fc19da81c51e3766bead6e2f2d5904
gin <73906888+gingkapls@users.noreply.github.com>
2024-12-17 14:10:31 +0530

    feat(UI): add drag and drop for ship

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,37 @@ const createBoard = (gridSize: number): HTMLDivElement => {
 const container = document.querySelector('#player1 > .board-container');
 const container2 = document.querySelector('#player2 > .board-container');
 
+const dragOverHandler = (e: DragEvent): void => {
+  e.preventDefault();
+  e.dataTransfer.dropEffect = 'move';
+  if (e.target instanceof HTMLElement) {
+    const data = e.dataTransfer.getData('text/plain');
+    const ship = document.querySelector(`#${data}`);
+
+    // Don't assign node to same cell
+    if (ship.parentElement === e.target.parentElement) return;
+
+    e.target.appendChild(ship);
+    console.log(e.target.dataset.row, e.target.dataset.col);
+  }
+};
+
+const dropHandler = (e: DragEvent): void => {
+  if (!(e.currentTarget instanceof HTMLElement)) return;
+
+  if (!(e.target instanceof HTMLElement)) return;
+};
+
+const ship = document.querySelector('#ship1');
+
+ship.addEventListener('dragstart', (e: DragEvent) => {
+  e.dataTransfer.setData('text/plain', (e.target as HTMLElement).id);
+  console.log((e.target as HTMLElement).id);
+});
+
 container.replaceChildren(createBoard(10));
 
+container.addEventListener('dragover', dragOverHandler);
+container.addEventListener('drop', dropHandler);
 
 container2.replaceChildren(createBoard(10));
