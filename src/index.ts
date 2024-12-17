d6b99508757fe7ae5b2fd55797cd4738433db791
gin <73906888+gingkapls@users.noreply.github.com>
2024-12-17 14:43:41 +0530

    feat(dom): improve ship drag and drop

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ const createBoard = (gridSize: number): HTMLDivElement => {
       cell.classList.add('grid-cell');
       cell.dataset.row = String(i);
       cell.dataset.col = String(j);
+      cell.draggable = false;
       board.appendChild(cell);
     }
   }
@@ -22,35 +23,53 @@ const container2 = document.querySelector('#player2 > .board-container');
 
 const dragOverHandler = (e: DragEvent): void => {
   e.preventDefault();
-  e.dataTransfer.dropEffect = 'move';
-  if (e.target instanceof HTMLElement) {
-    const data = e.dataTransfer.getData('text/plain');
-    const ship = document.querySelector(`#${data}`);
+  if (!(e.target instanceof HTMLElement)) return;
+  const data = e.dataTransfer.getData('text/plain');
+  const ship = document.querySelector(`#${data}`);
 
-    // Don't assign node to same cell
-    if (ship.parentElement === e.target.parentElement) return;
+  // Don't assign node to same cell
+  if (ship.parentElement === e.target.parentElement) return;
 
-    e.target.appendChild(ship);
-    console.log(e.target.dataset.row, e.target.dataset.col);
-  }
+  e.target.appendChild(ship);
+  console.log(e.target.dataset.row, e.target.dataset.col);
 };
 
 const dropHandler = (e: DragEvent): void => {
   if (!(e.currentTarget instanceof HTMLElement)) return;
-
   if (!(e.target instanceof HTMLElement)) return;
+
+  e.dataTransfer.dropEffect = 'move';
+};
+
+const dragStartHandler = (e: DragEvent): void => {
+  if (!(e.target instanceof HTMLElement)) return;
+
+  e.dataTransfer.setData('text/plain', e.target.id);
+  console.log(e.target.id);
+};
+
+const dragEndHandler = (e: DragEvent): void => {
+  if (!(e.currentTarget instanceof HTMLElement)) return;
+  if (!(e.target instanceof HTMLElement)) return;
+
+  console.log(e.dataTransfer.dropEffect);
+
+  if (e.dataTransfer.dropEffect === 'none') {
+    const data = e.dataTransfer.getData('text/plain');
+    const ship = document.querySelector(`#${data}`);
+
+    document.querySelector('.ship-container').append(ship);
+  }
 };
 
 const ship = document.querySelector('#ship1');
 
-ship.addEventListener('dragstart', (e: DragEvent) => {
-  e.dataTransfer.setData('text/plain', (e.target as HTMLElement).id);
-  console.log((e.target as HTMLElement).id);
-});
+ship.addEventListener('dragstart', dragStartHandler);
+ship.addEventListener('dragend', dragEndHandler);
 
+// Creating boards
 container.replaceChildren(createBoard(10));
+container2.replaceChildren(createBoard(10));
 
 container.addEventListener('dragover', dragOverHandler);
 container.addEventListener('drop', dropHandler);
-
-container2.replaceChildren(createBoard(10));
