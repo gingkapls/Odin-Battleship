eb16b9ef373eedd3ea45f227a356ba78cbc737d3
gin <73906888+gingkapls@users.noreply.github.com>
2024-12-21 02:08:54 +0530

    feat(Player): add methods

--- src/components/Player.ts
+++ src/components/Player.ts
@@ -1,13 +1,39 @@
-import { Gameboard } from './Gameboard';
+import { Gameboard, Pos } from './Gameboard';
+import { Ship } from './Ship';
 
 export class Player {
-  board: Gameboard;
+  #gameboard: Gameboard;
   name: string;
-  isTurn: boolean;
+  ships: Ship[];
 
-  constructor(board: Gameboard, name: string) {
-    this.board = board;
+  constructor(gameboard: Gameboard, name: string) {
+    this.#gameboard = gameboard;
     this.name = name;
-    this.isTurn = false;
+    this.ships = [
+      new Ship('Carrier', 'horizontal'),
+      new Ship('Battleship', 'horizontal'),
+      new Ship('Destroyer', 'horizontal'),
+      new Ship('Destroyer', 'horizontal'),
+      new Ship('Submarine', 'horizontal'),
+      new Ship('Submarine', 'horizontal'),
+      new Ship('Submarine', 'horizontal'),
+      new Ship('PatrolBoat', 'horizontal'),
+      new Ship('PatrolBoat', 'horizontal'),
+      new Ship('PatrolBoat', 'horizontal'),
+      new Ship('PatrolBoat', 'horizontal'),
+    ];
   }
+
+  get gameboard() {
+    return this.#gameboard;
+  }
+
+  placeShip(ship: Ship, [row, col]: Pos) {
+    const shipIndex = this.ships.indexOf(ship);
+    // const [shipToPlace] = this.ships.splice(shipIndex, 1);
+    const shipToPlace = this.ships.at(shipIndex);
+    this.gameboard.placeShip(shipToPlace, [row, col]);
+    console.log(this.gameboard);
+  }
+
 }
