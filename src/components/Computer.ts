09be011c918672faea26683919ea43c5a4f4980a
gin <73906888+gingkapls@users.noreply.github.com>
2024-12-29 14:09:47 +0530

    refactor(computer)

--- src/components/Computer.ts
+++ src/components/Computer.ts
@@ -1,50 +1,66 @@
-import { AttackResult, Gameboard, Pos } from './Gameboard';
+import { AttackResult, Pos } from './Gameboard';
 import { Player } from './Player';
 import { Ship } from './Ship';
 
 export class Computer {
-  #hitList: Map<string, AttackResult>;
+  #hitList: Map<string, AttackResult['status']>;
   player: Player;
+  opponent: Player;
 
-  constructor(player: Player) {
+  constructor(player: Player, opponent: Player) {
     this.player = player;
-    this.#hitList = new Map<string, AttackResult>();
+    this.player.name = 'Computer';
+    this.opponent = opponent;
+    this.#hitList = new Map<string, AttackResult['status']>();
   }
-  
+
   get gameboard() {
     return this.player.gameboard;
   }
 
-  #generateRandomCoords(): Pos {
+  get opponentBoard() {
+    return this.opponent.gameboard;
+  }
+
+  generateRandomCoords(): Pos {
     return [
-      Math.floor(Math.random() * this.gameboard.rows),
-      Math.floor(Math.random() * this.gameboard.cols),
+      Math.round(Math.random() * (this.gameboard.rows - 1)),
+      Math.round(Math.random() * (this.gameboard.cols - 1)),
     ];
   }
 
-  attackRandomCoord(): Pos {
+  attackRandomCoord(): AttackResult {
     let [row, col]: Pos = [0, 0];
     // Generate random coords that arent already attacked
     do {
-      [row, col] = this.#generateRandomCoords();
-    } while (!this.#hitList.has(`${row},${col}`));
+      [row, col] = this.generateRandomCoords();
+    } while (this.#hitList.has(`${row},${col}`));
 
-    const status = this.gameboard.receiveAttack([row, col]);
-    this.#hitList.set(`${row},${col}`, status);
+    // const result = gameController.attackPlayer(this.opponent, [row, col]);
+    const result = this.opponent.gameboard.receiveAttack([row, col]);
+    this.#hitList.set(`${row},${col}`, result.status);
 
-    return [0, 0];
+    return result;
   }
 
-  placeRandomShip(ship: Ship): Pos {
-    let areaStart: Pos;
-    let areaEnd: Pos;
+  randomizeAllShipOrientations() {
+    this.player.ships.forEach((ship) => {
+      if (Math.random() > 0.5) ship.toggleOrientation();
+    });
+  }
+
+  placeShipRandom(ship: Ship): Pos {
     let [row, col]: Pos = [0, 0];
     do {
-      [row, col] = this.#generateRandomCoords();
-      ({ areaStart, areaEnd } = Gameboard.vectorizeCoords(ship, [row, col]));
-    } while (!this.gameboard.isAreaEmpty(areaStart, areaEnd));
-    
+      [row, col] = this.generateRandomCoords();
+    } while (!this.gameboard.canPlaceShip(ship, [row, col]));
+
     this.player.placeShip(ship, [row, col]);
     return [row, col];
+  }
+
+  placeAllShips(): Pos[] {
+    this.randomizeAllShipOrientations();
+    return this.player.ships.map((ship) => this.placeShipRandom(ship));
   }
 }
