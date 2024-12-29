dd1d04b9cfff082d8b77c0ed406084738f53d039
gin <73906888+gingkapls@users.noreply.github.com>
2024-12-29 14:16:27 +0530

    feat: add driver code, add event listeners to game controller

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,62 @@
 // import { Computer } from './components/Computer';
-import { DOMBoardSetup } from './components/DOMBoardSetup';
-import { DragAndDropHandler } from './components/DragAndDropHandler';
-import { Gameboard } from './components/Gameboard';
+import { DOMBoard } from './components/DOMBoard';
+import { DOMController } from './components/DOMController';
+import { EventController } from './components/EventController';
+import { AttackResult } from './components/Gameboard';
+import { gameController, GameResult } from './components/GameController';
 import { Player } from './components/Player';
 import './style.css';
 
+const statusbar: HTMLElement = document.querySelector('.statusbar');
+const btnMode: HTMLInputElement = document.querySelector('#btn-mode');
+
 const player1Container: HTMLElement = document.querySelector(
-  '#player1.player-container',
+  '#player1.player-container > .board-container',
 );
+const handler1 = new EventController(gameController.player1);
+const board1 = new DOMBoard(gameController.player1, handler1, player1Container);
 
 const player2Container: HTMLElement = document.querySelector(
-  '#player2.player-container',
-)
-
-const player1 = new Player(new Gameboard(), 'gin');
-const handler = new DragAndDropHandler(player1);
-const board1 = new DOMBoardSetup(
-  player1,
-  handler,
-  player1Container,
+  '#player2.player-container > .board-container',
 );
+const handler2 = new EventController(gameController.player2);
+const board2 = new DOMBoard(gameController.player2, handler2, player2Container);
 
-const player2 = new Player(new Gameboard(), 'gin');
-const handler2 = new DragAndDropHandler(player1);
-const board2 = new DOMBoardSetup(
-  player2,
-  handler2,
-  player2Container,
-)
+const viewController = new DOMController(statusbar, btnMode, board1, board2);
+
+gameController.subscribe('playerReady', () => {
+  viewController.handleReady();
+});
+
+gameController.subscribe('changeTurn', (player: Player) => {
+  viewController.displayTurn(player);
+});
+
+gameController.subscribe('gameEnd', (result: GameResult) => {
+  console.log('game ended', result);
+  viewController.handleGameEnd(result);
+});
+
+gameController.subscribe('attack', (result: AttackResult) => {
+  viewController.handleAttack(result);
+})
+
+gameController.subscribe('changeTurn', (player: Player) => {
+  if (!gameController.isGameReady) return;
+  if (!gameController.isComputerEnabled) return;
+  if (player !== gameController.computer.player) return;
+
+  setTimeout(() => {
+
+  console.log('computer turn');
+  const status = gameController.playComputerTurn()
+  gameController.toggleTurn();
+  console.log('computer attack', status);
+
+  }, 500)
+})
+
+// TODO: Display attack status and turn at same time
+// gameController.subscribe('attack', (status: AttackResult) => {
+// viewController.displayAttackStatus(status);
+// })
